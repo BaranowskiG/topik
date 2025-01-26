f78ccb23029a196115d30d8f3d3a05c477c4251e
Grzegorz Baranowski <grzegorz.baranowski@olx.pl>
2025-01-26 17:07:56 +0100

    initial commit

---
+++ topik/topikApp.swift
@@ -0,0 +1,17 @@
+//
+//  topikApp.swift
+//  topik
+//
+//  Created by Grzegorz Baranowski on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct topikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
